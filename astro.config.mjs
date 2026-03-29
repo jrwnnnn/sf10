137ae1c20f5868316a7953e7b3fcb2a9fd394595
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2026-03-29 23:00:47 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'server',
+  adapter:
+		process.env.VERCEL === "1"
+			? vercel({})
+			: node({
+					mode: "standalone",
+				}),
+});

ee34a2fdcdc7a8972123676af29630c6df4ea2f6
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2026-04-14 13:57:34 +0800

    chore: Add Cloudflare adapter and Wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jrwnnnn.me",
-	vite: {
-		plugins: [tailwindcss()],
+  site: "https://jrwnnnn.me",
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
-	output: "static",
+
+  output: "static",
+  adapter: cloudflare(),
 });

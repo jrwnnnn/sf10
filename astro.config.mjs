0e11d910da035245a171a3331543b985c93c71cc
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2026-04-14 14:28:30 +0800

    style: Prettier code format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jrwnnnn.me",
-
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
 
-  output: "static",
-  adapter: cloudflare(),
+	output: "static",
+	adapter: cloudflare(),
 });

35c27459a90acb4ed426f6735c06caf44480381e
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2026-04-14 13:51:21 +0800

    chore: Removed Vercel adapter; removed @astrojs/vercel dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sf10.vercel.app",
+	site: "https://jrwnnnn.me",
 	vite: {
 		plugins: [tailwindcss()],
 	},
 	output: "static",
-	adapter: vercel({}),
 });

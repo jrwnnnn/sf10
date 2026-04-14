55b5de88a5b5936691be4d69dc3b41c276f77a7d
Mark Jerwin <ping@jrwnnnn.me>
2026-04-14 19:03:04 +0800

    chore: Add .wrangler to ESLint ignore patterns (#31)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
 
 export default [
-	{ ignores: [".astro/**", "dist/**", "node_modules/**"] },
+	{ ignores: [".astro/**", "dist/**", "node_modules/**", ".wrangler/**"] },
 	...tseslint.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
 ];
